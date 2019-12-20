d588b94b5e27ab4bbe776bc7715483123de35044
Tarpelite <tarpelite_chan@foxmail.com>
2019-12-20 21:22:39 +0800

    fix bugs

--- main.py
+++ main.py
@@ -45,12 +45,15 @@ def write(data, cnt):
     print("write exapmles to {}".format(dst))
     with open(dst, "w+", encoding="utf-8") as dst_f:
         for line in data:
-            print(line)
-            line_a = " ".join(line[0]) + "\n"
-            line_b = " ".join(line[1]) + "\n"
-            dst_f.write(line_a)
-            dst_f.write(line_b)
-            dst_f.write("\n")
+            try:
+                line_a = " ".join(line[0]) + "\n"
+                line_b = " ".join(line[1]) + "\n"
+                dst_f.write(line_a)
+                dst_f.write(line_b)
+                dst_f.write("\n")
+            except Exception as e:
+                print(line)
+                continue
 
 def process(data, tokenizer):
     id_list = []
